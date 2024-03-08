b88c3ef920d241b1d6fcf8bcd352f3fd4ba67535
jenil <149984350+jenilkhatrani@users.noreply.github.com>
2024-03-08 10:55:08 +0530

    third commit

---
+++ 6.5/6.5   2.dart
@@ -0,0 +1,15 @@
+void main() {
+  try
+  {
+    int result = 10 ~/ 0;
+    print('Result: $result');
+  }
+  catch (e)
+  {
+    print('Exception caught: $e');
+  }
+  finally
+  {
+    print('Finally block executed.');
+  }
+}
